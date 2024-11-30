cc31c9d7ff763bdec4e0e2930b9099e8b824df19
Muhammad Sukry <msukry21@gmail.com>
2024-11-30 23:55:35 +0700

    update dan build

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ItemLayout from "./ItemLayout";
 import Link from "next/link";
+import Image from "next/image";
 
 const AboutDetails = () => {
   return (
@@ -14,14 +15,14 @@ const AboutDetails = () => {
           <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
             Architect of Enchantment
           </h2>
-          <p className="font-light  text-xs sm:text-sm md:text-base   ">
-            "Every moment is a lesson, and every step forward is a chance to
-            evolve. I find beauty in how the world moves, driven by
+          <p className="font-light  text-xs sm:text-sm md:text-base">
+            &quot;Every moment is a lesson, and every step forward is a chance
+            to evolve. I find beauty in how the world moves, driven by
             technology—bridging gaps, solving problems, and shaping the future.
-            To me, it's not just about learning or working with technology; it's
-            about seeing the intricate dance of innovation and purpose that
-            powers the world around us. I’m here to understand it, improve it,
-            and grow with it." "still evolving"
+            To me, it&apos;s not just about learning or working with technology;
+            it&apos;s about seeing the intricate dance of innovation and purpose
+            that powers the world around us. I&apos;m here to understand it,
+            improve it, and grow with it.&quot; &quot;still evolving&quot;
           </p>
         </ItemLayout>
 
@@ -50,7 +51,7 @@ const AboutDetails = () => {
         {/* <ItemLayout
           className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
         >
-          <img
+          <Image
             className="w-full h-auto"
             src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="CodeBucks"
@@ -59,7 +60,7 @@ const AboutDetails = () => {
         </ItemLayout>
 
         <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
-          <img
+          <Image
             className="w-full h-auto"
             src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="CodeBucks"
@@ -68,7 +69,7 @@ const AboutDetails = () => {
         </ItemLayout> */}
 
         <ItemLayout className={"col-span-full"}>
-          <img
+          <Image
             className="w-full h-auto"
             src={`https://skillicons.dev/icons?i=bootstrap,css,figma,firebase,git,github,html,js,mysql,nextjs,nodejs,npm,postgres,react,supabase,tailwind,vercel,vite,vscode,py`}
             alt="CodeBucks"
@@ -77,7 +78,7 @@ const AboutDetails = () => {
         </ItemLayout>
 
         {/* <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
-          <img
+          <Image
             className="w-full h-auto"
             src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
             alt="CodeBucks"
@@ -91,7 +92,7 @@ const AboutDetails = () => {
             target="_blank"
             className="w-full"
           >
-            <img
+            <Image
               className="w-full h-auto"
               src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
               alt="CodeBucks"
